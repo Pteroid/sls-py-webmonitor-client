33f4ad17700e16989abe12cb1ceb0faaee700158
nakano <tohokamiemitame4185@gmail.com>
2018-05-08 08:48:39 +0900

    cognitoが動くところまで追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,40 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Dashboard from '@/components/Dashboard'
+import Login from '@/components/Login'
+import cognito from '@/cognito'
 
 Vue.use(Router)
 
+const requireAuth = (to, from, next) => {
+  cognito.isAuthenticated()
+    .then(session => {
+      next()
+    })
+    .catch(session => {
+      console.log(to.fullPath)
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath }
+      })
+    })
+}
+
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/dashboard'
+    },
+    {
+      path: '/dashboard',
+      component: Dashboard,
+      beforeEnter: requireAuth
+    },
+    {
+      path: '/login',
+      component: Login
     }
   ]
 })
